d38bd7bf757bd7c5e85ea6adc8da469ec1d39eab
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-01-29 11:19:14 +0100

    Add NAV + Person Cards + Footer

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ const personData = [
     {
         firstName: "Steven",
         lastName: "Fongue",
+        status: "Chef d'équipe",
         promo: "Master 2 - Informatique",
-        mySkills: ["", ""],
+        mySkills: ["Développeur"],
         photo: "https://via.placeholder.com/150"
     },
     {
-        firstName: "Valentin",
+        firstName: "Corentin",
         lastName: "Guillard",
         promo: "Bachelor 2 - Informatique",
-        mySkills: ["", ""],
+        mySkills: ["Développeur Full Stack - Junior"],
         photo: "https://via.placeholder.com/150"
     },
     {
@@ -24,47 +25,59 @@ const personData = [
         firstName: "Mathis",
         lastName: "Le Roy-Nivot",
         promo: "Bachelor 2 - Informatique",
-        mySkills: ["", ""],
+        mySkills: ["Développeur Full Stack - Junior", "Montage Vidéo"],
         photo: "https://via.placeholder.com/150"
     },
     {
         firstName: "Corentin",
         lastName: "Lemarchand",
         promo: "Bachelor 1 - 3D Animations",
-        mySkills: ["", ""],
+        mySkills: ["Modélisation 3D"],
         photo: "https://via.placeholder.com/150"
     },
     {
         firstName: "Julien",
         lastName: "Macillet",
         promo: "Master 1 - Communication 360",
-        mySkills: ["", ""],
+        mySkills: ["Marketing", "Communication"],
         photo: "https://via.placeholder.com/150"
     },
     {
         firstName: "Kévin",
         lastName: "Maheo",
         promo: "Master 2 - Informatique (Réseaux)",
-        mySkills: ["", ""],
+        mySkills: ["Administration des systèmes"],
         photo: "https://via.placeholder.com/150"
     },
     {
         firstName: "Clémentine",
         lastName: "Pouillet",
         promo: "Bachelor 3 - Design",
-        mySkills: ["", ""],
+        mySkills: ["Design"],
+        photo: "https://via.placeholder.com/150"
+    }, 
+    {
+        firstName: "Alix",
+        lastName: "Nouzillat",
+        promo: "Master 1 - Chef de projet Web",
+        mySkills: ["Développeur Full Stack"],
         photo: "https://via.placeholder.com/150"
     }
 ];
 
-
-
+function statut(status) {
+    return `
+<p class="test">
+${status.map(status => `${status}`).join("")}
+</p>
+`;
+}
 
 function skills(skills) {
     return `
 <h4>Compétences</h4>
 <ul class="skills-list">
-${skills.map(skills => `<li>${skills}</li>`).join("")}
+${skills.map(skills => `<li class="skills">${skills}</li>`).join("")}
 </ul>
 `;
 }
@@ -80,8 +93,15 @@ function personTemplate(person) {
   `;
 }
 
-document.getElementById("app").innerHTML = `
+document.getElementById("group-members").innerHTML = `
   ${personData.map(personTemplate).join("")}
-  <p class="footer">${personData.length} personnes composent le groupe n°18</p>
 `;
 
+document.getElementById("footer-content").innerHTML = `
+<div class="footer">${personData.length} personnes composent le groupe #18 - Les Dix Huitres</div>
+<div class="social-networks">
+    <a href="#f" target="_blank"><i class="fab fa-facebook-square"></i></a>
+    <a href="#t" target="_blank"><i class ="fab fa-twitter-square"></i></a>
+    <a href="#i" target="_blank"><i class="fab fa-instagram"></i></a>
+</div>
+`;
